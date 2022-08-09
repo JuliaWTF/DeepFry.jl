3e56d429a1b106f00d81b1d69648c9c470dbe1e1
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-08-09 09:46:42 +0200

    Typing and passing rng around

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,16 @@ using Random: default_rng
 
 @testset "DeepFry.jl" begin
     img = TestImages.testimage("mountainstream")
-    # Write your tests here.
+    @testset "Test structure frying" begin
+        for (name, f) in DeepFry.STRUCTURE_FRYING
+            @test_nowarn f(default_rng(), img)
+        end
+    end
+    @testset "Test color frying" begin
+        for (name, f) in DeepFry.COLOR_FRYING
+            @test_nowarn f(default_rng(), img)
+        end
+    end
 end
 
 
@@ -16,5 +25,3 @@ deepfry(img)
 
 # mosaicview(pushfirst!([f(default_rng(), img) for (n, f) in pairs(DeepFry.STRUCTURE_FRYING)], img);nrow=2, fillvalue=colorant"white")
 # mosaicview(pushfirst!([f(default_rng(), img) for (n, f) in pairs(DeepFry.COLOR_FRYING)], img);nrow=2, fillvalue=colorant"white")
-    
-end
